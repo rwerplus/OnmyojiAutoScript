6be7f539df3cd9ad4612b3a08056e5395e4a84d6
chubbycat <noko@gmail.com>
2024-08-16 21:53:08 +0800

    fix(#388): 更新题库

--- tasks/DemonEncounter/data/json_to_csv.py
+++ tasks/DemonEncounter/data/json_to_csv.py
@@ -10,6 +10,9 @@ def run():
     # 定义一个正则表达式模式，用于匹配所有的标点符号和特殊符号
     pattern = r'[^\w\s]'
 
+    # 创建一个集合来存储唯一的(问题, 答案)对
+    unique_entries = set()
+
     # 打开一个CSV文件准备写入数据
     with open('data.csv', mode='w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
@@ -20,6 +23,10 @@ def run():
             answer = str(item.get('answer', ''))
             clean_question = re.sub(pattern, '', question)
             clean_answer = re.sub(pattern, '', answer)
-            writer.writerow([clean_question, clean_answer])
+
+            # 使用一个集合来去除重复项
+            if (clean_question, clean_answer) not in unique_entries:
+                unique_entries.add((clean_question, clean_answer))
+                writer.writerow([clean_question, clean_answer])
 if __name__ == "__main__":
     run()
